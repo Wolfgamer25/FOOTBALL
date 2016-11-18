cccf045a147f0514692f089b2a14a32013541fac
Jonathan Torres <jonathantorres@Jonathans-MacBook-Pro.local>
2016-11-18 08:16:15 -0500

    hello

--- canvas.js
+++ canvas.js
@@ -4,90 +4,78 @@ var ballX = 400;
 var ballSpeedX = 5;
 var ballY = 175;
 var ballSpeedY = 5;
-window.onload = function(){
-  canvas = document.getElementById('gameCanvas');
-  canvasContext = canvas.getContext('2d');
-  drawEveryThing();
-  var framesPerSecond = 30;
-setInterval(updateAll, 1000/framesPerSecond); //speed of the ball
+var keys = [];
+window.onload = function() {
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    drawEveryThing();
+    var framesPerSecond = 40;
+    setInterval(updateAll, 1000 / framesPerSecond); //speed of the ball
 
 }
-function updateAll(){ //updates functions fps
-  drawEveryThing();
-  moveEveryThing();
-  keysPlayer1();
+
+function updateAll() { //updates functions fps
+    drawEveryThing();
+    moveEveryThing();
+    keysPlayer();
+
+
+
+}
+
+
+
+function ballReset() {
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+}
+
+function drawEveryThing() {
+    //draws the soccerField
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    paddleRightSide();
+    drawCircle(ballX, ballY, 9, 'white');
+    da();
+
+
+}
+
+function drawCircle(centerX, centerY, radius, fillColor) {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
+
+function moveEveryThing() {
+    ballX += ballSpeedX; //evertime the screen update the ball is going get updated
+    ballY += ballSpeedY;
+    box();
+
+}
+
+
+function box(){
+  if (ballY < 0) { // top canvas bounce
+      ballSpeedY *= -1;
+  }
+  if (ballY > canvas.height) { //bottom canvas bounce
+      ballSpeedY *= -1;
+  }
+  if (ballX < 0) { //left canvas bounce
+      ballSpeedX *= -1; //ballReset();
+  }
+  if (ballX > canvas.width) {
+      ballSpeedX *= -1; //ballReset();
   }
 
 
 
-function ballReset(){
-  ballX = canvas.width/2;
-  ballY = canvas.height/2;
 }
-function drawEveryThing(){
-  //draws the soccerField
-  canvasContext.fillStyle = 'green';
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  paddleRightSide();
-  drawCircle(ballX, ballY, 9, 'white');
-  da();
-
-
-}
-function drawCircle(centerX, centerY, radius, fillColor){
-  canvasContext.fillStyle = fillColor;
-  canvasContext.beginPath();
-  canvasContext.arc(centerX,centerY, radius, 0, Math.PI * 2, true);
-  canvasContext.fill();
-}
-
-function moveEveryThing(){
-  ballX+= ballSpeedX;  //evertime the screen update the ball is going get updated
-  ballY+= ballSpeedY;
-  if(ballY < 0){   // top canvas bounce
-    ballSpeedY *= -1;
-  }
-  if(ballY > canvas.height){ //bottom canvas bounce
-    ballSpeedY *= -1;
-  }
-  if(ballX < 0){ //left canvas bounce
-    ballSpeedX *= -1; //ballReset();
-  }
-  if(ballX > canvas.width){
-    ballSpeedX *= -1; //ballReset();
-  }
-}
-function keysPlayer1(){
-  document.body.onkeydown=function(event){
-if (event.keyCode === 81){
-   //player 1 defenders up Q
-    goalie.y-=distance;
-  }
-  if (event.keyCode === 83){ // player 1 defenders down S
-    goalie.y +=distance;
-   }
-   if (event.keyCode === 87){
-    // player 1 middle up W
-      stickR1.y -= distance;
-      stickR1.y1 -= distance;
-  }
-    if (event.keyCode === 68){
-       //player 1 middle down d
-      stickR1.y += distance;
-      stickR1.y1 += distance;
-    }
-    if (event.keyCode === 69){
-      stickR2.y -= distance;
-}
-    if (event.keyCode === 70){
-      stickR2.y += distance;
-}   if (event.keyCode === 82){
-      stickR3.y -= distance;
-}   if (event.keyCode === 71){
-      stickR3.y += distance;
-}
-}
-}
-function keysPlayer2(){
-
-}
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
--- players.js
+++ players.js
@@ -1,76 +1,188 @@
+//red team
 var goalie = {
-  x:20,
-  y:135,
-  width:20,
-  height:30,
-  color:'red',
+    x: 20,
+    y: 135,
+    width: 20,
+    height: 30,
+    color: 'red',
 }
 var stickR1 = {
-  x: 170,    //STICKS
-  y:75,
-  width:20,
-  height:150,
-  test:'white',
-  y1:195,
-  color:'red',
-  height1:30,
+    x: 170, //STICKS
+    y: 75,
+    width: 20,
+    height: 150,
+    test: 'white',
+    color: 'red',
+    height1: 30,
 
 
 }
 var stickR2 = {
-  x:350,
-  y:25,
-  width:20,
-  height: 280,
-  test:'white',
-  color: 'red',
-  height1: 20,
+    x: 350,
+    y: 25,
+    width: 20,
+    height: 280,
+    test: 'white',
+    color: 'red',
+    height1: 20,
 }
 var stickR3 = {
-  x:550,
-  y:35,
-  width:20,
+    x: 550,
+    y: 35,
+    width: 20,
+    height: 230,
+    test: 'white',
+    color: 'red',
+    height1: 30,
+}
+//blue team
+var goalieB = {
+    x: 770,
+    y: 135,
+    width: 20,
+    height: 30,
+    color: 'blue',
+}
+var stickB1 = {
+    x: 620, //STICKS
+    y: 75,
+    width: 20,
+    height: 150,
+    test: 'white',
+    color: 'blue',
+    height1: 30,
+}
+var stickB2 = {
+  x: 450, //STICKS
+  y: 25,
+  width: 20,
+  height: 280,
+  test: 'white',
+  color: 'blue',
+  height1: 20,
+}
+var stickB3 = {
+  x: 260, //STICKS
+  y: 35,
+  width: 20,
   height: 230,
-  test:'white',
-  color: 'red',
+  test: 'white',
+  color: 'blue',
   height1: 30,
 }
 var distance = 20;
 
+function keysPlayer() {
+    document.body.onkeydown = function(event) {
+        if (keys[81] && goalie.y > 0) {
+            //goalie up q
+            goalie.y -= distance;
+        }
+        if (keys[65] && goalie.y < canvas.height-goalie.height*1.2) { // player 1 defenders down S
+            // goalie down a
+            goalie.y += distance;
+        }
+        if (keys[87] && stickR1.y > 0 ) {
+            // defendersR up w
+            stickR1.y -= distance;
+            stickR1.y1 -= distance;
+        }
+        if (keys[83] && stickR1.y < canvas.height- stickR1.height*1.1) {
+            //defendersR down s
+            stickR1.y += distance;
+            stickR1.y1 += distance;
+        }
+        if (keys[69] && stickR2.y > 10) {
+            //midR up e
+            stickR2.y -= distance;
+        }
+        if (keys[68] && stickR2.y <canvas.height - stickR2.height*1.05) {
+            //midR down d
+            stickR2.y += distance;
+        }
+        if (keys[82] && stickR3.y > 0) {
+            //strikerR up r
+            stickR3.y -= distance;
+        }
+        if (keys[70] && stickR3.y <canvas.height - stickR3.height*1.1) {
+            //strikerR down f
+            stickR3.y += distance;
+        }
+          if (keys[74] && goalieB.y > 0) {
+              //goalieB  up  j
+              goalieB.y -= distance;
+          }
+          if (keys[77] && goalieB.y < canvas.height-goalieB.height*1.2) { // player 1 defenders down S
+              //goalieB down m
+              goalieB.y += distance;
+          }
+          if (keys[75] && stickB1.y > 0 ) {
+              // defendersB up k
+              stickB1.y -= distance;
 
+          }
+          if (keys[188] && stickB1.y < canvas.height- stickB1.height*1.1) {
+              //defendersB down ,
+              stickB1.y += distance;
 
-function paddleLeftSide(){
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(770, 135, 20, 30)}
-  function rowB1(){
-  canvasContext.fillStyle = 'white';     //STICKS
-  canvasContext.fillRect(620,75, 20, 150);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(620, 195, 20, 30);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(620, 75, 20, 30);
-} function rowB2(){
-  canvasContext.fillStyle = 'white';     //STICKS
-  canvasContext.fillRect(450,25, 20, 280);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(450, 25, 20, 20);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(450, 90, 20, 20);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(450, 155, 20, 20);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(450, 220, 20, 20);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(450, 285, 20, 20);
-} function rowB3(){
-  canvasContext.fillStyle = 'white';     //STICKS
-  canvasContext.fillRect(260,35, 20, 230);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(260, 135, 20, 30);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(260, 235, 20, 30);
-  canvasContext.fillStyle = 'blue';
-  canvasContext.fillRect(260, 35, 20, 30);
+          }
+          if (keys[76] && stickB2.y > 10) {
+              // midB up l
+              stickB2.y -= distance;
+          }
+          if (keys[190] && stickB2.y <canvas.height - stickB2.height*1.05) {
+              //midB down .
+              stickB2.y += distance;
+          }
+          if (keys[186] && stickB3.y > 0) {
+              //strikerB up ;
+              stickB3.y -= distance;
+          }
+          if (keys[191] && stickB3.y <canvas.height - stickB3.height*1.1) {
+              //strikerB down;
+              stickB3.y += distance;
+          }
+      }
+    }
+
+function paddleLeftSide() {
+    canvasContext.fillStyle = goalieB.color;
+    canvasContext.fillRect(goalieB.x, goalieB.y, goalieB.width, goalieB.height)
+}
+
+function rowB1() {
+    canvasContext.fillStyle = stickB1.test; //STICKS
+    canvasContext.fillRect(stickB1.x, stickB1.y, stickB1.width, stickB1.height);
+    canvasContext.fillStyle = stickB1.color;
+    canvasContext.fillRect(stickB1.x,stickB1.y+120, stickB1.width, stickB1.height1);
+    canvasContext.fillStyle = stickB1.color;
+    canvasContext.fillRect(stickB1.x, stickB1.y, stickB1.width, stickB1.height1);
+}
+
+function rowB2() {
+    canvasContext.fillStyle = stickB2.test; //STICKS
+    canvasContext.fillRect(stickB2.x, stickB2.y, stickB2.width, stickB2.height);
+    canvasContext.fillStyle = stickB2.color;
+    canvasContext.fillRect(stickB2.x, stickB2.y, stickB2.width, stickB2.height1);
+    canvasContext.fillStyle = stickB2.color;
+    canvasContext.fillRect(stickB2.x, stickB2.y + 65, stickB2.width, stickB2.height1);
+    canvasContext.fillStyle = stickB2.color;
+    canvasContext.fillRect(stickB2.x, stickB2.y +130, stickB2.width, stickB2.height1);
+    canvasContext.fillStyle = stickB2.color;
+    canvasContext.fillRect(stickB2.x, stickB2.y +195, stickB2.width, stickB2.height1);
+    canvasContext.fillStyle = stickB2.color;
+    canvasContext.fillRect(stickB2.x, stickB2.y +260, stickB2.width, stickB2.height1);
+}
+
+function rowB3() {              35
+    canvasContext.fillStyle = stickB3.test; //STICKS
+    canvasContext.fillRect(stickB3.x, stickB3.y, stickB3.width, stickB3.height);
+    canvasContext.fillStyle = stickB3.color;
+    canvasContext.fillRect(stickB3.x, stickB3.y + 100, stickB3.width, stickB3.height1);
+    canvasContext.fillStyle = stickB3.color;
+    canvasContext.fillRect(stickB3.x, stickB3.y + 200, stickB3.width, stickB3.height1);
+    canvasContext.fillStyle = stickB3.color;
+    canvasContext.fillRect(stickB3.x, stickB3.y, stickB3.width, stickB3.height1);
 }
 // var player1 = {
 // goal:paddleLeftSide,
@@ -78,49 +190,53 @@ function paddleLeftSide(){
 // mid:row2(),
 // striker:row3
 // }
-function da(){ //for draw
-  paddleLeftSide();
-  row1();
-  row2();
-  row3();
-  paddleRightSide();
-  rowB1();
-  rowB2();
-  rowB3();
+function da() { //for draw
+    paddleLeftSide();
+    row1();
+    row2();
+    row3();
+    paddleRightSide();
+    rowB1();
+    rowB2();
+    rowB3();
 }
-function paddleRightSide(){
-  canvasContext.fillStyle = goalie.color;
-  canvasContext.fillRect(goalie.x, goalie.y, goalie.width, goalie.height);
+
+function paddleRightSide() {
+    canvasContext.fillStyle = goalie.color;
+    canvasContext.fillRect(goalie.x, goalie.y, goalie.width, goalie.height);
 }
-function row1(){
-  canvasContext.fillStyle = stickR1.test;     //STICKS
-  canvasContext.fillRect(stickR1.x,stickR1.y, stickR1.width, stickR1.height);
-  canvasContext.fillStyle = stickR1.color;
-  canvasContext.fillRect(stickR1.x, stickR1.y1, stickR1.width, stickR1.height1);
-  canvasContext.fillStyle = stickR1.color;
-  canvasContext.fillRect(stickR1.x,stickR1.y,stickR1.width,stickR1.height1);
+
+function row1() {
+    canvasContext.fillStyle = stickR1.test; //STICKS
+    canvasContext.fillRect(stickR1.x, stickR1.y, stickR1.width, stickR1.height);
+    canvasContext.fillStyle = stickR1.color;
+    canvasContext.fillRect(stickR1.x, stickR1.y + 120, stickR1.width, stickR1.height1);
+    canvasContext.fillStyle = stickR1.color;
+    canvasContext.fillRect(stickR1.x, stickR1.y, stickR1.width, stickR1.height1);
 };
 
-function row2(){
-  canvasContext.fillStyle = stickR2.test;     //STICKS
-  canvasContext.fillRect(stickR2.x,stickR2.y, stickR2.width, stickR2.height);
-  canvasContext.fillStyle = stickR2.color;
-  canvasContext.fillRect(stickR2.x, stickR2.y, stickR2.width, stickR2.height1);
-  canvasContext.fillStyle = stickR2.color;
-  canvasContext.fillRect(stickR2.x, stickR2.y+65, stickR2.width, stickR2.height1);
-  canvasContext.fillStyle = stickR2.color;
-  canvasContext.fillRect(stickR2.x, stickR2.y+130, stickR2.width, stickR2.height1);
-  canvasContext.fillStyle = stickR2.color;
-  canvasContext.fillRect(stickR2.x, stickR2.y+195, stickR2.width, stickR2.height1);
-  canvasContext.fillStyle = stickR2.color;
-  canvasContext.fillRect(stickR2.x, stickR2.y+260, stickR2.width, stickR2.height1);
-} function row3(){
-  canvasContext.fillStyle = stickR3.test;     //STICKS
-  canvasContext.fillRect(stickR3.x,stickR3.y, stickR3.width, stickR3.height);
-  canvasContext.fillStyle = stickR3.color;
-  canvasContext.fillRect(stickR3.x, stickR3.y+200, stickR3.width, stickR3.height1);
-  canvasContext.fillStyle = stickR3.color;
-  canvasContext.fillRect(stickR3.x, stickR3.y, stickR3.width, stickR3.height1);
-  canvasContext.fillStyle = stickR3.color;
-  canvasContext.fillRect(stickR3.x, stickR3.y + 100, stickR3.width, stickR3.height1);
+function row2() {
+    canvasContext.fillStyle = stickR2.test; //STICKS
+    canvasContext.fillRect(stickR2.x, stickR2.y, stickR2.width, stickR2.height);
+    canvasContext.fillStyle = stickR2.color;
+    canvasContext.fillRect(stickR2.x, stickR2.y, stickR2.width, stickR2.height1);
+    canvasContext.fillStyle = stickR2.color;
+    canvasContext.fillRect(stickR2.x, stickR2.y + 65, stickR2.width, stickR2.height1);
+    canvasContext.fillStyle = stickR2.color;
+    canvasContext.fillRect(stickR2.x, stickR2.y + 130, stickR2.width, stickR2.height1);
+    canvasContext.fillStyle = stickR2.color;
+    canvasContext.fillRect(stickR2.x, stickR2.y + 195, stickR2.width, stickR2.height1);
+    canvasContext.fillStyle = stickR2.color;
+    canvasContext.fillRect(stickR2.x, stickR2.y + 260, stickR2.width, stickR2.height1);
+}
+
+function row3() {
+    canvasContext.fillStyle = stickR3.test; //STICKS
+    canvasContext.fillRect(stickR3.x, stickR3.y, stickR3.width, stickR3.height);
+    canvasContext.fillStyle = stickR3.color;
+    canvasContext.fillRect(stickR3.x, stickR3.y + 200, stickR3.width, stickR3.height1);
+    canvasContext.fillStyle = stickR3.color;
+    canvasContext.fillRect(stickR3.x, stickR3.y, stickR3.width, stickR3.height1);
+    canvasContext.fillStyle = stickR3.color;
+    canvasContext.fillRect(stickR3.x, stickR3.y + 100, stickR3.width, stickR3.height1);
 }
